1a9e0850554ada62aba4d0b2c667780dcd4a5246
Federica <federica.comuni@gmail.com>
2020-02-25 19:24:04 +0100

    Day 1

---
+++ day1/day1.hs
@@ -0,0 +1,27 @@
+task1 :: FilePath -> IO Int
+task1 f = do numFile <- readFile f
+             let nums = lines numFile
+             let numbers = [calculateCost (read n :: Int) | n <- nums]
+             return $ sum numbers
+
+calculateCost :: Int -> Int
+calculateCost num = num `div` 3 - 2                 
+                  
+                  
+task2 :: FilePath -> IO Int
+task2 f = do numFile <- readFile f
+             let nums = lines numFile
+             let numbers = [calculateCostRe (read n :: Int) | n <- nums]
+             return $ sum numbers
+             
+             
+calculateCostRe :: Int -> Int
+calculateCostRe num | num' <= 0  = 0
+                    | otherwise  = num' + calculateCostRe num'
+              where num' = num `div` 3 - 2 
+
+
+
+
+
+
