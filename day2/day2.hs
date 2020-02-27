093026e992a9df3c3d974b1c711520d1c0647cc6
Federica <federica.comuni@gmail.com>
2020-02-27 23:08:52 +0100

    day 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,19 +1,34 @@
 import Data.List.Split
 
-task1 :: FilePath -> IO [Int]
+task1 :: FilePath -> IO Int
 task1 f = do numFile <- readFile f
              let nums = [read n :: Int | n <- splitOn "," numFile]
-             let n_nums = [replaceAt (nums!!(n + 3)) (calculate n nums) nums | n <- [0..length nums], 
-                                                                               n `mod` 4 == 0,
-                                                                               nums!!n /= 99]
-             return $ last n_nums
+             return $ head $ solve (replaceAt 2 2 (replaceAt 1 12 nums)) 0
+             
 
-calculate :: Int -> [Int] -> Int
-calculate n nums = case nums!!n of
-                   1 -> nums!!(n + 1) + nums!!(n + 2)
-                   2 -> nums!!(n + 1) * nums!!(n + 2)
+solve :: [Int] -> Int -> [Int]
+solve l n = case l!!n of 
+            1 -> solve (replaceAt (l!!(n + 3)) (calculate (+) n l) l) (n + 4)
+            2 -> solve (replaceAt (l!!(n + 3)) (calculate (*) n l) l) (n + 4)
+            99 -> l          
+          
+calculate :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+calculate op n l = op (l!!(l!!(n + 1))) (l!!(l!!(n + 2)))  
 
 
-replaceAt :: Int -> a -> [a] -> [a]
+replaceAt :: Int -> Int -> [Int] -> [Int]
 replaceAt n val (x:xs) | n == 0    = val:xs
                        | otherwise = x:replaceAt (n - 1) val xs
+                       
+                       
+task2 :: FilePath -> IO Int
+task2 f = do numFile <- readFile f
+             let nums = [read n :: Int | n <- splitOn "," numFile]
+             let n_tries = [solve (replaceAt 2 x (replaceAt 1 y nums)) 0 | x <- [0..100], y <- [0..100]]
+             let find = findCorrect n_tries
+             return $ 100 * find!!1 + find!!2
+             
+             
+findCorrect :: [[Int]] -> [Int]
+findCorrect (x:xs) | head x == 19690720 = x
+                   | otherwise          = findCorrect xs
