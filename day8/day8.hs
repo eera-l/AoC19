7cb38f40ccad4eb7ded30f243149d5bf43e76c8f
eera-l <federica.comuni@gmail.com>
2020-05-01 10:42:47 +0200

    day 8

--- day8/day8.hs
+++ day8/day8.hs
@@ -15,34 +15,34 @@ task1 :: FilePath -> IO Int
 task1 f = do digits <- readFile f
              let nums     = map digitToInt digits
              let fullList = readLayerz (readLinez nums width) height
-             return $ countOneTwoz fullList (head $ returnIndex $ findLeastZeros fullList)
+             return $ countOneTwoz fullList $ returnIndex $ findLeastZeros fullList
              
              
 readLinez :: [a] -> Int -> [[a]]
-readLinez [] _ = [[]]
-readLinez l 0  = [[]]
+readLinez [] _ = error "Empty list"
+readLinez l 0  = error "Enter number greater than 0"
 readLinez l n  = chunksOf n l
 
 
 readLayerz :: [[a]] -> Int -> [[[a]]]
-readLayerz [[]] _ = [[[]]]
-readLayerz l 0    = [[[]]]
+readLayerz [[]] _ = error "Empty list"
+readLayerz l 0    = error "Enter number greater than 0"
 readLayerz l n    = chunksOf n l
 
 
 findLeastZeros :: [[[Int]]] -> [(Int, Int)]
-findLeastZeros [[[]]] = []
-findLeastZeros l      = [(sum [length (filter (\x -> x == 0) line) | line <- l!!i], i) | i <- [0..(length l) - 1]]
+findLeastZeros [[[]]] = error "Empty list"
+findLeastZeros l      = [(sum [length (filter (0==) line) | line <- l!!i], i) | i <- [0..(length l) - 1]]
 
 
-returnIndex :: [(Int, Int)] -> [Int]
-returnIndex [] = []
-returnIndex l  = [snd x | x <- l, fst x == (minimum (fst $ unzip l))]
+returnIndex :: [(Int, Int)] -> Int
+returnIndex [] = error "Empty list"
+returnIndex l  = head [snd x | x <- l, fst x == (minimum (fst $ unzip l))]
 
 
 countOneTwoz :: [[[Int]]] -> Int -> Int
-countOneTwoz [[[]]] _ = (-1)
-countOneTwoz l n      = sum [length (filter (\x -> x == 1) line) | line <- l!!n] * sum [length (filter (\x -> x == 2) line) | line <- l!!n]
+countOneTwoz [[[]]] _ = error "Empty list"
+countOneTwoz l n      = sum [length (filter (1==) line) | line <- l!!n] * sum [length (filter (2==) line) | line <- l!!n]
 
 
 task2 :: FilePath -> IO ()
@@ -54,7 +54,7 @@ task2 f = do digits <- readFile f
 
 
 colorize :: [Int] -> Char
-colorize []                  = 'x'
+colorize []                  = error "Empty list"
 colorize (x:xs)  | x == 0    = ' '
                  | x == 1    = 'o'
                  | otherwise = colorize xs                  
